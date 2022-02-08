cf06cb669f6f707b807ce99861199731a6458195
MohanramAnbukkarasu <44049406+MohanramAnbukkarasu@users.noreply.github.com>
2022-02-08 15:36:05 +0530

    Add files via upload

---
+++ SfDataGrid_GettingStarted/Form1.cs
@@ -0,0 +1,34 @@
+﻿using SfDataGrid_Demo;
+using Syncfusion.WinForms.Controls;
+using Syncfusion.WinForms.DataGrid;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Windows.Forms;
+using Syncfusion.WinForms.DataGrid.Enums;
+using System.Drawing;
+using Syncfusion.WinForms.DataGrid.Renderers;
+using Syncfusion.WinForms.GridCommon.ScrollAxis;
+using Syncfusion.Data.Extensions;
+
+namespace SfDataGrid_Demo
+{
+    /// <summary>
+    /// Summary description for Form1.
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Initializes the new instance for the Form.
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            sfDataGrid1.DataSource = new OrderInfoCollection().OrdersListDetails;
+        }
+
+        #endregion
+    }
+}
